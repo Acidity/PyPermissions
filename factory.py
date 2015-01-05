a1df1b739c09f26c1146d69eb7d0ea547fcf2676
Tyler O'Meara <Tyler@TylerOMeara.com>
2015-01-05 01:29:41 -0600

    Update factories to utilize templates.
    Signed-off-by: Tyler O'Meara <Tyler@TylerOMeara.com>

--- factory.py
+++ factory.py
@@ -1,11 +1,11 @@
-from permission import Permission, WildcardPermission
+from permission import Permission
 
 
 class PermissionFactory(object):
     """General factory for creating permissions. Will select the correct permission type for the provided string and
         return that."""
 
-    def __init__(self, delimiter=".", wildcard="*", prefix=""):
+    def __init__(self, delimiter=".", wildcard="*", prefix="", templates=None):
         """Allows developers to use custom characters for the special characters in the permissions.
 
         :param delimiter: The character that serves as the separator for permissions. Default value: "."
@@ -16,6 +16,7 @@ class PermissionFactory(object):
         self.delimiter = delimiter
         self.wildcard = wildcard
         self.prefix = prefix + self.delimiter if prefix else ""
+        self.templates = templates if templates else list()
 
     def create_permission(self, permission):
         """Create a permission from the provided string, adding prefixes as necessary.
@@ -27,10 +28,12 @@ class PermissionFactory(object):
         if self.prefix:
             permission = self.prefix + permission
 
-        if WildcardPermission.meets_requirements(permission):
-            return WildcardPermission(permission)
-        if Permission.meets_requirements(permission, wildcard=self.wildcard):
-            return Permission(permission)
+        for template in self.templates:
+            if template.matches_format(permission)[0]:
+                return template.create_permission(permission)
+
+        return Permission(name=permission, delimiter=self.delimiter, wildcard=self.wildcard)
+
 
     def create_child(self, prefix):
         """Create a PermissionFactory with a prefix that is this factory's prefix with the provided prefix added on.
@@ -39,9 +42,20 @@ class PermissionFactory(object):
         :param prefix: The prefix to be added to the end of this factory's prefix
         """
 
-        return PermissionFactory(delimiter=self.delimiter,
-                                 wildcard=self.wildcard,
-                                 prefix=self.prefix + prefix)
+        return PermissionFactory(delimiter=self.delimiter, wildcard=self.wildcard, prefix=self.prefix + prefix,
+                                 templates=self.templates)
+
+    def register_template(self, template, index=None):
+        """Register a new template for the factory to use when creating permissions.
+
+        :param template: the :py:class:`PermissionTemplate` to be added
+        """
+
+        if not index:
+            self.templates.append(template)
+        else:
+            self.templates.insert(index, template)
+
 
     def __repr__(self):
         return "{cls}({prefix}, {d}, {w})".format(cls=self.__class__.__name__, prefix=self.prefix[:-1], w=self.wildcard,
